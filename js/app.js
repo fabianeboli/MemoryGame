f8ec3ae6addb461a71fbbbb9884ba245b67e22fe
Fabian <fe6321@gmail.com>
2018-01-09 16:28:17 +0100

    Added animations to cards

--- js/app.js
+++ js/app.js
@@ -3,29 +3,18 @@ $(function () {
     $("#submit").on("click",function (event) {
         var NUMCOL = $("#NumCol").val();
         var NUMROW = $("#NumRows").val();
-        $("table").remove();
-        let table = document.createElement("table");
+        buildTable(NUMCOL,NUMROW);
 
-        document.body.appendChild(table);
-        //$("table").attr("id","table");
-        for(var i = 0; i<NUMROW;i++){
-            $("table").append("<tr>");
-            for(var j = 0;j<NUMCOL;j++){
-                $("tr").last().append("<td>");
-                //table.appendChild(document.createElement("td"));
-            }
-        }
-        // event.preventDefault();
+        $(document).on("click",".flip-container",function () {
+           $(this).toggleClass('hover');
+        });
         event.preventDefault();
     })
 });
-
-
-
 // --------------------------------Card Class-------------------------------------------------
-var Card = function (name,img) {
+var Card = function (name,front) {
     this.name = name;
-    this.img = img;
+    this.front = front;
     this.back = 'images/cardsBack/Background.jpg';
 }
     Card.prototype = {
@@ -35,10 +24,21 @@ var Card = function (name,img) {
     AddToArray: function (arr,imgs) {
             var card = new Card("card"+i,imgs[i]);
             arrayOfCards.push(card);
-            console.log(card + " " + card.name + " " + card.img + " " + card.back);
-    }
-}
-
+            console.log(card + " " + card.name + " " + card.front + " " + card.back);
+    },
+      turn: function () {
+          $(".turn").click(function () {
+              var src;
+              if(src === undefined || src === this.front){
+                  src = this.back;
+              } else {
+                  src = this.front;
+              }
+              turnCSS(this, src);
+          });
+      }
+};
+//--------------------------------------------------------------------------------------------
 var arrayOfCards = [];
 var imgs = ['images/dog-hovawart-black-pet-89775-min.png',
     'images/dolphin-marine-mammals-water-sea-64219-min.png',
@@ -51,20 +51,49 @@ var imgs = ['images/dog-hovawart-black-pet-89775-min.png',
 
 addAllCardsToArray(arrayOfCards,imgs);
 
-
-
-//$("body").css("background-image","url('images/dog-hovawart-black-pet-89775-min.png')");
-
-
-function addAllCardsToArray (arr,imgs) {
-    for(var i = 0;i<imgs.length;i++){
-        var card = new Card("card"+i,imgs[i]);
-        //card.name = "card"+i;
-        //card.img = imgs[i];
-        arrayOfCards.push(card);
-        console.log(card + " " + card.name + " " + card.img + " " + card.back);
+function buildTable(NumOfRows,NumofCols) {
+    $("table").remove();
+    var table = document.createElement("table");
+    document.body.appendChild(table);
+    for(var i = 0; i<NumOfRows;i++){
+        $("table").append("<tr>");
+        for(var j = 0;j<NumofCols;j++){
+            $("tr").last().append("<td>");
+            $("td").last().prepend("<div class='flip-container' onclick='this.classList.toggle(hover)'>");
+            $(".flip-container").last().prepend("<div class='flipper'>");
+            $(".flipper").last().prepend("<div class='back'>");
+            $(".back").last().prepend("<img class="+arrayOfCards[j].name+" src="+arrayOfCards[j].front+" >");
+            $(".back").last().after("<div class='front'>");
+            $(".front").last().prepend("<img class="+arrayOfCards[j].name+" src="+arrayOfCards[j].back+" >");
+        }
     }
 }
+
+function addAllCardsToArray (arr,imgs) {
+    for(let i = 0;i<imgs.length;i++){
+        let card = new Card("Card"+i,imgs[i]);
+        arrayOfCards.push(card);
+        console.log(card + " " + card.name + " " + card.front + " " + card.back);
+    }
+}
+
+function turnCSS(elem) {
+    $(elem)
+        .addClass("flipping")
+        .bind("transitionend webkittransitionend", function () { //should add more prefixes
+            let src = $(elem).attr("src");
+            if(src === undefined || src === elem.front){
+                $(this).attr("src",elem.back);
+            } else {
+               $(this).attr("src",elem.front);
+            }
+            $(this)
+                .unbind("transitionend webkittransitionend")
+                .removeClass("flipping")
+        })
+}
+
+
 
 
 
